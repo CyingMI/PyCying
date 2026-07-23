a8e9e7d5de7cfc13522e9f4a2a7dc7a8405d0191
wangh950 <1679689986@qq.com>
2026-07-23 18:23:22 +0800

    update atomic model

--- cying/nn/modules/__init__.py
+++ cying/nn/modules/__init__.py
@@ -1,20 +1,19 @@
 from .conv import Conv1d, Conv2d, Conv3d
 from .spectral_conv import SpectralConv1d, SpectralConv2d, SpectralConv3d
-from .equivariant_conv import EquivariantConv3d
 from .equivariant_spectral_conv import EquivariantSpectralConv3d
 from .operator_layer import OperatorLayer1d, OperatorLayer2d, OperatorLayer3d
 from .operator_model import OperatorModel1d, OperatorModel2d, OperatorModel3d
 from .lang_layer import LangLayer
 from .lang_model import LangModel
-from .atomic_layer import AtomicLayer3d
+from .atomic_layer import AtomicLayer
 from .atomic_model import AtomicModel
 
 __all__ = [
     'Conv1d', 'Conv2d', 'Conv3d',
     'SpectralConv1d', 'SpectralConv2d', 'SpectralConv3d',
-    'EquivariantConv3d', 'EquivariantSpectralConv3d',
+    'EquivariantSpectralConv3d',
     'OperatorLayer1d', 'OperatorLayer2d', 'OperatorLayer3d',
     'OperatorModel1d', 'OperatorModel2d', 'OperatorModel3d',
     'LangLayer', 'LangModel',
-    'AtomicLayer3d', 'AtomicModel'
+    'AtomicLayer', 'AtomicModel'
 ]
--- cying/nn/modules/atomic_model.py
+++ cying/nn/modules/atomic_model.py
@@ -1,57 +1,96 @@
-import numpy as np
-import math
 import torch
 import torch.nn as nn
-from .atomic_layer import AtomicLayer3d
+from .atomic_layer import AtomicLayer
+from .equivariant_spectral_conv import EFNO3d
+
 
 class AtomicModel(nn.Module):
-    def __init__(self, size, params, cut_off_r=6):
+    def __init__(self, d_model, hid_width, num_layers, cutoff_r = 6):
         super().__init__()
-        self.size = size
-        self.params = params
-        self.cut_off_r = cut_off_r
-        self.atomic_model = nn.Sequential(*[
-            AtomicLayer3d(self.size, **param) for param in self.params
+        self.d_model = d_model
+        self.hid_width = hid_width
+        self.num_layers = num_layers
+        self.cutoff_r = cutoff_r
+
+        self.ato_emb = nn.Embedding(119, d_model, 0)
+
+        self.layers = nn.ModuleList([
+            AtomicLayer(d_model, hid_width) for _ in range(num_layers)
         ])
 
-        q1 = torch.fft.fftfreq(size[0], d=1/size[0])
-        q2 = torch.fft.fftfreq(size[1], d=1/size[1])
-        q3 = torch.fft.rfftfreq(size[2], d=1/size[2])
-        q1, q2, q3 = torch.meshgrid(q1, q2, q3, indexing='ij')
-        self.register_buffer('q', torch.stack((q1, q2, q3), dim=-1))
-        q_norm = (self.q**2).sum(dim=-1).int()
-        self.register_buffer('key', torch.searchsorted(q_norm.unique(), q_norm))
+        self.ene_lin = nn.Linear(d_model, 1)
 
-        self.emb = nn.Embedding(119, q_norm.unique().numel())
-
-        self.out_linear = nn.Linear(self.params[-1]['out_channels'], 1)
-
-        self.norm = nn.RMSNorm(self.size)
-
-
-    def forward(self, cell, ato_num, ato_pos):
-        ato_emb = self.emb(ato_num)[:,self.key] + 1j*0
-        # 局部坐标
-        scaled_pos = ato_pos @ torch.inverse(cell)
-        rel_scaled_pos = scaled_pos[None,...] - scaled_pos[:,None,:]
-        rel_scaled_pos = rel_scaled_pos - torch.round(rel_scaled_pos)
-        rel_pos = torch.einsum('N M i, i j -> N M j', rel_scaled_pos, cell)
+    def forward(self, ato_num, scaled_pos, cell):
+        rel_scaled_pos = scaled_pos[:,None,...] - scaled_pos[:,:,None,:]
+        rel_scaled_pos = rel_scaled_pos - torch.round(rel_scaled_pos) * (rel_scaled_pos.abs() < 2)
+        rel_pos = torch.einsum('B N M i, B i j -> B N M j', rel_scaled_pos, cell)
         rel_dis = rel_pos.norm(dim=-1)
-        ato_mask = (rel_dis < self.cut_off_r) * self.sp(rel_dis/self.cut_off_r) + 1j * 0
 
-        V_fre = torch.exp(-1j * torch.einsum('N M d, x y z d -> N M x y z', rel_pos, self.q))
-        V_fre = torch.einsum('N M x y z, N M, M x y z -> N x y z', V_fre, ato_mask, ato_emb) * math.prod(self.size)
-        V_rel = self.norm(torch.fft.irfftn(V_fre, dim=(-3,-2,-1)))
+        rel_pos = rel_pos / (rel_dis + (rel_dis < 1e-2))[...,None]
+        rel_pos = torch.cat([rel_pos, torch.ones_like(rel_pos[...,:1])], dim=-1)
+        srij = self.s5(rel_dis) * (rel_dis > 1e-2)
 
-        V_rel = self.atomic_model(V_rel[:,None]).mean(dim=[2,3,4])
+        rel_pos = rel_pos * (srij / (rel_dis + (rel_dis < 1e-2)))[...,None]
 
-        energy = self.out_linear(V_rel).sum()
+        mul_pos = torch.einsum('B N M d, B N O d -> B N M O', rel_pos, rel_pos)
 
-        return energy
+        ato_emb = self.ato_emb(ato_num)
+
+        for layer in self.layers:
+            ato_emb = layer(ato_emb, mul_pos)
+
+        return self.ene_lin(ato_emb)
     
-    def sp(self, r, p=5):
-        ap = - (p+1) * (p+2) * (p+3) / 6
-        bp = p * (p + 2) * (p + 3) / 2
-        cp = - p * (p + 1) * (p + 3) / 2
-        dp = p * (p + 1) * (p + 2) / 6
-        return (1 + r**p * (ap + bp * r + cp * r**2 + dp * r**3)) * (r < 1)
+    def s5(self, x):
+        x = x / self.cutoff_r
+        return (1 - x**5*(56 - 140*x + 120*x**2 - 35*x**3)) * (x < 1)
+
+# class AtomicModel(nn.Module):
+#     def __init__(self, C, L, num_layers, cutoff_r = 6):
+#         super().__init__()
+#         self.C = C
+#         self.L = L
+#         self.num_layers = num_layers
+#         self.cutoff_r = cutoff_r
+
+#         q = torch.fft.fftfreq(L, 1/L)
+#         k1, k2, k3 = torch.meshgrid(q, q, q, indexing='ij')
+#         self.register_buffer('k', torch.stack([k1, k2, k3], dim=-1) * torch.pi / cutoff_r)
+#         k_square = (k1**2 + k2**2 + k3**2).int()
+#         opt_len = k_square.unique().numel()
+#         self.key = torch.searchsorted(k_square.unique(), k_square)
+
+#         self.atomic_emb = nn.Embedding(119, C, 0)
+
+#         self.layers = nn.ModuleList([
+#             AtomicLayer(C, L, opt_len, self.key) for _ in range(num_layers)
+#         ])
+#         self.ene_weight = nn.Parameter(torch.zeros(opt_len, C), requires_grad=True)
+
+#     def forward(self, ato_num, scaled_pos, cell):
+#         ato_emb = self.atomic_emb(ato_num)
+#         rel_scaled_pos = scaled_pos[:,None,...] - scaled_pos[:,:,None,:]
+#         rel_scaled_pos = rel_scaled_pos - torch.round(rel_scaled_pos) * (rel_scaled_pos.abs() < 2)
+#         rel_pos = torch.einsum('B N M i, B i j -> B N M j', rel_scaled_pos, cell)
+#         rel_dis = rel_pos.norm(dim=-1)
+
+#         global_mask = (rel_dis < self.cutoff_r).float()
+#         radial_mask = self.s5(rel_dis) * (rel_dis > 1e-2)
+#         expijk = torch.cos(torch.einsum('i j k d, B N M d -> B N M i j k', self.k, rel_pos))
+#         weight = self.ene_weight[self.key].permute(3, 0, 1, 2)
+#         energy = torch.einsum(
+#             'B N M i j k, B N C, B M C, C i j k, B N M -> B N i j k',
+#             expijk,
+#             ato_emb,
+#             ato_emb,
+#             weight,
+#             global_mask
+#         ).mean(dim=[-3,-2,-1])
+#         for layer in self.layers:
+#             energy, ato_emb = layer(energy, ato_emb, expijk, radial_mask, global_mask)
+
+#         return energy
+    
+#     def s5(self, x):
+#         x = x / self.cutoff_r
+#         return (1 - x**5*(56 - 140*x + 120*x**2 - 35*x**3)) * (x < 1)
