4de51adc3913dde3b6f80d322e125466adb157cb
wangh950 <1679689986@qq.com>
2026-07-24 16:38:28 +0800

    fix data prepreprocessing issues

--- cying/nn/modules/atomic_layer.py
+++ cying/nn/modules/atomic_layer.py
@@ -6,69 +6,50 @@ class AtomicLayer(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.hid_width = hid_width
-        # self.q_linear = nn.Linear(d_model, d_model, bias=False)
-        # self.k_linear = nn.Linear(d_model, d_model, bias=False)
+
         self.opt_weight = nn.Parameter(torch.zeros(d_model // 2 + 1, 2), requires_grad=True)
+        self.qk_linear = nn.Linear(2*d_model, d_model, bias=False)
         self.v_linear = nn.Linear(d_model, d_model, bias=False)
 
-        self.ffn = nn.Sequential(
+        self.ene_fnn = nn.Sequential(
             nn.Linear(d_model, hid_width),
             nn.GELU(),
             nn.Linear(hid_width, d_model)
         )
 
-        self.ato_norm = nn.RMSNorm(d_model)
+        self.env_fnn = nn.Sequential(
+            nn.Linear(d_model, hid_width),
+            nn.GELU(),
+            nn.Linear(hid_width, d_model)
+        )
 
-        self.out_norm = nn.RMSNorm(d_model)
+        self.env_norm = nn.RMSNorm(d_model)
 
-    def forward(self, ato_emb, mul_pos):
-        # ato_emb: B*N*d, mul_pos: B*N*M*O
-        ato_mul = torch.einsum('B N d, B M d -> B N M d', ato_emb, ato_emb)
+        self.envout_norm = nn.RMSNorm(d_model)
+
+        self.ene_norm = nn.RMSNorm(d_model)
+
+        self.eneout_norm = nn.RMSNorm(d_model)
+
+    def forward(self, ene_emb, env_emb, mul_pos, srij):
+        qk = self.qk_linear(torch.cat([ene_emb[...,None,:].expand(*env_emb.shape),env_emb], dim=-1))
         opt_weight = torch.view_as_complex(self.opt_weight)
-        ato_mul_fre = torch.fft.rfft(ato_mul)
-        v = self.v_linear(ato_emb)
+        env_fre = torch.fft.rfft(qk)
+        v = self.v_linear(env_emb)
 
         s = torch.einsum(
-            'B N M d, B N O d, d, B N M O -> B N O', 
-            ato_mul_fre,
-            ato_mul_fre.conj(),
-            opt_weight, 
-            mul_pos + 1j*0
-        ).real / self.d_model
+            'B N M d, B N O d, d -> B N M O',
+            env_fre,
+            env_fre.conj(),
+            opt_weight
+        ).real / self.d_model * mul_pos
 
-        env_emb = torch.einsum('B N M, B M d -> B N d', s, v)
+        env_emb = self.env_norm(torch.einsum('B N M O, B N O d, B N O -> B N M d', s, v, srij) + env_emb)
 
-        return self.out_norm(self.ffn(self.ato_norm(ato_emb + env_emb)) + ato_emb)
+        env_emb = self.envout_norm(self.env_fnn(env_emb) + env_emb)
 
-# class AtomicLayer(nn.Module):
-#     def __init__(self, C, L, opt_len, key):
-#         super().__init__()
-#         self.C = C
-#         self.L = L
-#         self.opt_len = opt_len
-#         self.key = key
-#         self.ene_weight = nn.Parameter(torch.zeros(opt_len, C), requires_grad=True)
-#         self.opt_weight = nn.Parameter(torch.zeros(opt_len, C, C), requires_grad=True)
+        ene_emb = self.ene_norm(torch.einsum('B N M d, B N M -> B N d', env_emb, srij) + ene_emb)
 
-#     def forward(self, energy, ato_emb, expijk, radial_mask, global_mask):
-#         weight = self.opt_weight[self.key].permute(3, 4, 0, 1, 2)
-#         env_emb = torch.einsum(
-#             'B N M i j k, B N C, B M C, C O i j k, B N M -> B N O i j k',
-#             expijk,
-#             ato_emb,
-#             ato_emb,
-#             weight,
-#             radial_mask
-#         ).mean(dim=[-3,-2,-1])
-#         ato_emb = ato_emb + env_emb
+        ene_emb = self.eneout_norm(self.ene_fnn(ene_emb) + ene_emb)
 
-#         weight = self.ene_weight[self.key].permute(3, 0, 1, 2)
-#         energy = torch.einsum(
-#             'B N M i j k, B N C, B M C, C i j k, B N M -> B N i j k',
-#             expijk,
-#             ato_emb,
-#             ato_emb,
-#             weight,
-#             global_mask
-#         ).mean(dim=[-3,-2,-1]) + energy
-#         return energy, ato_emb
+        return ene_emb, env_emb
--- cying/nn/modules/atomic_model.py
+++ cying/nn/modules/atomic_model.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 from .atomic_layer import AtomicLayer
 from .equivariant_spectral_conv import EFNO3d
 
-
 class AtomicModel(nn.Module):
     def __init__(self, d_model, hid_width, num_layers, cutoff_r = 6):
         super().__init__()
@@ -20,77 +19,37 @@ class AtomicModel(nn.Module):
 
         self.ene_lin = nn.Linear(d_model, 1)
 
-    def forward(self, ato_num, scaled_pos, cell):
-        rel_scaled_pos = scaled_pos[:,None,...] - scaled_pos[:,:,None,:]
-        rel_scaled_pos = rel_scaled_pos - torch.round(rel_scaled_pos) * (rel_scaled_pos.abs() < 2)
-        rel_pos = torch.einsum('B N M i, B i j -> B N M j', rel_scaled_pos, cell)
+    def forward(self, ato_num, ato_pos, neb_matrix, cell_shifts, cell):
+        env_num = torch.take_along_dim(
+            ato_num[:,None,:],
+            neb_matrix,
+            dim=2
+        )
+        neb_pos = torch.take_along_dim(
+            ato_pos[:,None,:],
+            neb_matrix[...,None],
+            dim=2
+        ) + torch.einsum('B N M i, B i j -> B N M j', cell_shifts, cell)
+        rel_pos = neb_pos - ato_pos[...,None,:]
         rel_dis = rel_pos.norm(dim=-1)
 
         rel_pos = rel_pos / (rel_dis + (rel_dis < 1e-2))[...,None]
         rel_pos = torch.cat([rel_pos, torch.ones_like(rel_pos[...,:1])], dim=-1)
-        srij = self.s5(rel_dis) * (rel_dis > 1e-2)
+        srij = self.s5(rel_dis) * (rel_dis > 1e-2) * (env_num > 0)
 
-        rel_pos = rel_pos * (srij / (rel_dis + (rel_dis < 1e-2)))[...,None]
+        rel_pos = rel_pos / (rel_dis + (rel_dis < 1e-2))[...,None]
 
         mul_pos = torch.einsum('B N M d, B N O d -> B N M O', rel_pos, rel_pos)
 
-        ato_emb = self.ato_emb(ato_num)
+        ene_emb = self.ato_emb(ato_num)
+        env_emb = self.ato_emb(env_num)
 
         for layer in self.layers:
-            ato_emb = layer(ato_emb, mul_pos)
-
-        return self.ene_lin(ato_emb)
+            ene_emb, env_emb = layer(ene_emb,env_emb,mul_pos,srij)
+        
+        return self.ene_lin(ene_emb)
     
     def s5(self, x):
         x = x / self.cutoff_r
+        x = x * (x < 1)
         return (1 - x**5*(56 - 140*x + 120*x**2 - 35*x**3)) * (x < 1)
-
-# class AtomicModel(nn.Module):
-#     def __init__(self, C, L, num_layers, cutoff_r = 6):
-#         super().__init__()
-#         self.C = C
-#         self.L = L
-#         self.num_layers = num_layers
-#         self.cutoff_r = cutoff_r
-
-#         q = torch.fft.fftfreq(L, 1/L)
-#         k1, k2, k3 = torch.meshgrid(q, q, q, indexing='ij')
-#         self.register_buffer('k', torch.stack([k1, k2, k3], dim=-1) * torch.pi / cutoff_r)
-#         k_square = (k1**2 + k2**2 + k3**2).int()
-#         opt_len = k_square.unique().numel()
-#         self.key = torch.searchsorted(k_square.unique(), k_square)
-
-#         self.atomic_emb = nn.Embedding(119, C, 0)
-
-#         self.layers = nn.ModuleList([
-#             AtomicLayer(C, L, opt_len, self.key) for _ in range(num_layers)
-#         ])
-#         self.ene_weight = nn.Parameter(torch.zeros(opt_len, C), requires_grad=True)
-
-#     def forward(self, ato_num, scaled_pos, cell):
-#         ato_emb = self.atomic_emb(ato_num)
-#         rel_scaled_pos = scaled_pos[:,None,...] - scaled_pos[:,:,None,:]
-#         rel_scaled_pos = rel_scaled_pos - torch.round(rel_scaled_pos) * (rel_scaled_pos.abs() < 2)
-#         rel_pos = torch.einsum('B N M i, B i j -> B N M j', rel_scaled_pos, cell)
-#         rel_dis = rel_pos.norm(dim=-1)
-
-#         global_mask = (rel_dis < self.cutoff_r).float()
-#         radial_mask = self.s5(rel_dis) * (rel_dis > 1e-2)
-#         expijk = torch.cos(torch.einsum('i j k d, B N M d -> B N M i j k', self.k, rel_pos))
-#         weight = self.ene_weight[self.key].permute(3, 0, 1, 2)
-#         energy = torch.einsum(
-#             'B N M i j k, B N C, B M C, C i j k, B N M -> B N i j k',
-#             expijk,
-#             ato_emb,
-#             ato_emb,
-#             weight,
-#             global_mask
-#         ).mean(dim=[-3,-2,-1])
-#         for layer in self.layers:
-#             energy, ato_emb = layer(energy, ato_emb, expijk, radial_mask, global_mask)
-
-#         return energy
-    
-#     def s5(self, x):
-#         x = x / self.cutoff_r
-#         return (1 - x**5*(56 - 140*x + 120*x**2 - 35*x**3)) * (x < 1)
