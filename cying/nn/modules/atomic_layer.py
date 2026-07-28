e7f274d214fe600e92e0500aa04d92ff5aa16f30
wangh950 <1679689986@qq.com>
2026-07-28 18:09:29 +0800

    update local atomic model

--- cying/nn/modules/atomic_layer.py
+++ cying/nn/modules/atomic_layer.py
@@ -26,107 +26,45 @@ class AtomicLayer(nn.Module):
         )
         length = k_square_unique.size(0)
 
-        self.register_buffer('key', key)
+        self.ato_weight = nn.Parameter(torch.zeros(length, d_model, d_model), requires_grad=True)
 
-        self.opt_weight = nn.Parameter(torch.zeros(length, d_model), requires_grad=True)
+        self.env_weight = nn.Parameter(torch.zeros(length, d_model, d_model), requires_grad=True)
 
-        # self.opt_linear = nn.Linear(d_model, d_model, bias=False)
+        self.env_linear = nn.Linear(2*d_model, d_model, bias=False)
 
-        self.fnn = nn.Sequential(
+        self.fnn_ato = nn.Sequential(
             nn.Linear(d_model, hid_width),
             SIREN(),
             nn.Linear(hid_width, d_model)
         )
 
-        self.norm_in = nn.RMSNorm(d_model)
-        self.norm_out = nn.RMSNorm(d_model)
-
-    def forward(self, ato_emb, srij, expijk, neb_matrix):
-        env_emb = torch.take_along_dim(
-            ato_emb[:,None,:],
-            neb_matrix[...,None],
-            dim=2
+        self.fnn_env = nn.Sequential(
+            nn.Linear(d_model, hid_width),
+            SIREN(),
+            nn.Linear(hid_width, d_model)
         )
 
-        w = self.opt_weight[self.key]
+        self.env_norm_in = nn.RMSNorm(d_model)
+        self.env_norm_out = nn.RMSNorm(d_model)
+        
+        self.ato_norm_in = nn.RMSNorm(d_model)
+        self.ato_norm_out = nn.RMSNorm(d_model)
 
-        fij = torch.einsum('i j k n, B N M i j k -> B N M n', w, expijk) / self.L**3
+    def forward(self, ato_emb, ato_env, env_field, expijk):
+        ato_env = self.env_linear(torch.cat([ato_env,ato_emb[...,None,:].expand(*ato_env.shape)],dim=-1))
 
-        env = torch.einsum('B N M n, B N M n, B N n, B N M -> B N n', fij, env_emb, ato_emb, srij)
+        env_induc = torch.einsum('B N n s, B N M n, s n m, B N M s -> B N M m', env_field, ato_env + 1j*0, self.env_weight + 1j*0, expijk.conj()).real / self.L**3
 
-        ato_emb = self.norm_in(env + ato_emb)
+        ato_env = self.env_norm_in(env_induc + ato_env)
 
-        return self.norm_out(self.fnn(ato_emb) + ato_emb)
+        ato_env = self.env_norm_out(self.fnn_env(ato_env) + ato_env)
 
-# class SIREN(nn.Module):
-#     def __init__(self):
-#         super().__init__()
+        env_field = torch.einsum('B N M d, B N M s -> B N d s', ato_env + 1j*0, expijk)
 
-#     def forward(self, x):
-#         return torch.sin(x)
+        ato_induc = torch.einsum('B N n s, B N n, s n m -> B N m', env_field.real, ato_emb, self.ato_weight) / self.L**3
 
-# class AtomicLayer(nn.Module):
-#     def __init__(self, d_model, hid_width):
-#         super().__init__()
-#         self.d_model = d_model
-#         self.hid_width = hid_width
+        ato_emb = self.ato_norm_in(ato_induc + ato_emb)
 
-#         self.opt_weight = nn.Parameter(torch.zeros(d_model // 2 + 1, 2), requires_grad=True)
-#         self.qk_linear = nn.Linear(2*d_model, d_model, bias=False)
-#         self.v_linear = nn.Linear(d_model, d_model, bias=False)
+        ato_emb = self.ato_norm_out(self.fnn_ato(ato_emb) + ato_emb)
 
-#         self.ene_fnn = nn.Sequential(
-#             nn.Linear(d_model, hid_width),
-#             nn.GELU(),
-#             nn.Linear(hid_width, d_model)
-#         )
-
-#         self.env_fnn = nn.Sequential(
-#             nn.Linear(d_model, hid_width),
-#             nn.GELU(),
-#             nn.Linear(hid_width, d_model)
-#         )
-
-#         self.gr = nn.Sequential(
-#             nn.Linear(1, 32),
-#             SIREN(),
-#             nn.Linear(32, 32),
-#             SIREN(),
-#             nn.Linear(32, 32),
-#             SIREN(),
-#             nn.Linear(32, 1)
-#         )
-
-#         self.env_norm = nn.RMSNorm(d_model)
-
-#         self.envout_norm = nn.RMSNorm(d_model)
-
-#         self.ene_norm = nn.RMSNorm(d_model)
-
-#         self.eneout_norm = nn.RMSNorm(d_model)
-
-#     def forward(self, ene_emb, env_emb, rel_pos, rel_dis, srij):
-#         rel_pos = rel_pos * self.gr(rel_dis[...,None])
-#         mul_pos = torch.einsum('B N M d, B N O d -> B N M O', rel_pos, rel_pos)
-
-#         qk = self.qk_linear(torch.cat([ene_emb[...,None,:].expand(*env_emb.shape),env_emb], dim=-1))
-#         opt_weight = torch.view_as_complex(self.opt_weight)
-#         env_fre = torch.fft.rfft(qk)
-#         v = self.v_linear(env_emb)
-
-#         s = torch.einsum(
-#             'B N M d, B N O d, d -> B N M O',
-#             env_fre,
-#             env_fre.conj(),
-#             opt_weight
-#         ).real / self.d_model * mul_pos
-
-#         env_emb = self.env_norm(torch.einsum('B N M O, B N O d, B N O -> B N M d', s, v, srij) + env_emb)
-
-#         env_emb = self.envout_norm(self.env_fnn(env_emb) + env_emb)
-
-#         ene_emb = self.ene_norm(torch.einsum('B N M d, B N M -> B N d', env_emb, srij) + ene_emb)
-
-#         ene_emb = self.eneout_norm(self.ene_fnn(ene_emb) + ene_emb)
-
-#         return ene_emb, env_emb
+        return ato_emb, ato_env, env_field
--- cying/nn/modules/atomic_model.py
+++ cying/nn/modules/atomic_model.py
@@ -13,7 +13,17 @@ class AtomicModel(nn.Module):
 
         k = torch.fft.fftfreq(L, 1/L)
         k1, k2, k3 = torch.meshgrid(k, k, k, indexing='ij')
-        self.register_buffer('k', torch.stack([k1, k2, k3],dim=-1) * torch.pi / cutoff_r)
+        k = torch.stack([k1, k2, k3],dim=-1)
+        k2_flat = (k**2).sum(dim=-1).flatten().int()
+        k2_unique, key = torch.unique(
+            k2_flat, 
+            return_inverse=True,
+            return_counts=False
+        )
+        self.opt_size = len(k2_unique)
+
+        self.register_buffer('key', key)
+        self.register_buffer('k', k * torch.pi / cutoff_r)
 
         self.ato_emb = nn.Embedding(119, d_model, 0)
 
@@ -30,6 +40,12 @@ class AtomicModel(nn.Module):
             neb_matrix,
             dim=2
         )
+        ato_env = torch.take_along_dim(
+            ato_emb[:,None,:],
+            neb_matrix[...,None],
+            dim=2
+        )
+
         neb_pos = torch.take_along_dim(
             ato_pos[:,None,:],
             neb_matrix[...,None],
@@ -38,12 +54,25 @@ class AtomicModel(nn.Module):
         rel_pos = neb_pos - ato_pos[...,None,:]
         rel_dis = rel_pos.norm(dim=-1)
 
-        srij = self.s5(rel_dis) * (rel_dis > 1e-2) * (env_num > 0)
+        srij = self.s5(rel_dis) * (rel_dis > 1e-2) * (env_num > 0) + 1j*0
 
-        expijk = torch.cos(torch.einsum('B N M d, i j k d -> B N M i j k', rel_pos, self.k))
+        expijk = torch.exp(-1j*torch.einsum('B N M d, i j k d -> B N M i j k', rel_pos, self.k))
+
+        B, N, M, i, j, k = expijk.shape
+
+        key = self.key[None,None,None,...]
+        key = key.expand(B, N, M, -1)
+        
+        expijk_ = torch.zeros(B, N, M, self.opt_size, dtype=expijk.dtype, device=expijk.device)
+
+        expijk_.scatter_add_(dim=-1, index=key, src=expijk.flatten(-3,-1))
+
+        expijk = expijk_ * srij[...,None]
+
+        env_field = torch.einsum('B N M d, B N M s -> B N d s', ato_env + 1j*0, expijk)
 
         for layer in self.layers:
-            ato_emb = layer(ato_emb, srij, expijk, neb_matrix)
+            ato_emb, ato_env, env_field = layer(ato_emb, ato_env, env_field, expijk)
         
         return self.ene_lin(ato_emb)
     
@@ -51,50 +80,3 @@ class AtomicModel(nn.Module):
         x = x / self.cutoff_r
         x = x * (x < 1)
         return (1 - x**5*(56 - 140*x + 120*x**2 - 35*x**3)) * (x < 1)
-
-# class AtomicModel(nn.Module):
-#     def __init__(self, d_model, hid_width, num_layers, cutoff_r = 6):
-#         super().__init__()
-#         self.d_model = d_model
-#         self.hid_width = hid_width
-#         self.num_layers = num_layers
-#         self.cutoff_r = cutoff_r
-
-#         self.ato_emb = nn.Embedding(119, d_model, 0)
-
-#         self.layers = nn.ModuleList([
-#             AtomicLayer(d_model, hid_width) for _ in range(num_layers)
-#         ])
-
-#         self.ene_lin = nn.Linear(d_model, 1)
-
-#     def forward(self, ato_num, ato_pos, neb_matrix, cell_shifts, cell):
-#         env_num = torch.take_along_dim(
-#             ato_num[:,None,:],
-#             neb_matrix,
-#             dim=2
-#         )
-#         neb_pos = torch.take_along_dim(
-#             ato_pos[:,None,:],
-#             neb_matrix[...,None],
-#             dim=2
-#         ) + torch.einsum('B N M i, B i j -> B N M j', cell_shifts, cell)
-#         rel_pos = neb_pos - ato_pos[...,None,:]
-#         rel_dis = rel_pos.norm(dim=-1)
-
-#         rel_pos = rel_pos / (rel_dis + (rel_dis < 1e-2))[...,None]
-#         rel_pos = torch.cat([rel_pos, torch.ones_like(rel_pos[...,:1])], dim=-1)
-#         srij = self.s5(rel_dis) * (rel_dis > 1e-2) * (env_num > 0)
-
-#         ene_emb = self.ato_emb(ato_num)
-#         env_emb = self.ato_emb(env_num)
-
-#         for layer in self.layers:
-#             ene_emb, env_emb = layer(ene_emb,env_emb,rel_pos,rel_dis,srij)
-        
-#         return self.ene_lin(ene_emb)
-    
-#     def s5(self, x):
-#         x = x / self.cutoff_r
-#         x = x * (x < 1)
-#         return (1 - x**5*(56 - 140*x + 120*x**2 - 35*x**3)) * (x < 1)
