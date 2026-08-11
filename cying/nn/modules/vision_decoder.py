fe090abdd1b54a4b11eaaf5a004592031854eb01
wangh950 <1679689986@qq.com>
2026-08-11 16:17:55 +0800

    update vision decoder module

--- cying/nn/modules/vision_decoder.py
+++ cying/nn/modules/vision_decoder.py
@@ -4,58 +4,51 @@ from .vision_decoder_layer import VisionDecoderLayer
 
 class VisionDecoder(nn.Module):
     def __init__(
-            self,
-            d_model,
-            num_heads,
-            hidden_width,
-            num_layers,
-            num_query = 100
-        ):
+        self,
+        d_model,
+        num_heads,
+        hidden_width,
+        num_layers,
+        target_len = 64,
+        query_len = 100
+    ):
         super().__init__()
-        self.layers = nn.ModuleList([VisionDecoderLayer(d_model,
-                                                        num_heads,
-                                                        hidden_width
-                                                        ) 
-                                     for _ in range(num_layers)]
-                                     )
-
-        self.query_seq = nn.Parameter(torch.randn(num_query, d_model))
-        
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.hidden_width = hidden_width
+        self.num_layers = num_layers
+        self.target_len = target_len
+        self.query_len = query_len
         self.d_head = d_model // num_heads
 
-        self.q_position_embeddings = torch.exp(1j * nn.Parameter(
-            torch.randn(num_query, self.d_head // 2 + 1) * 0.02
-        ).unsqueeze(1)
+        self.layers = nn.ModuleList([
+            VisionDecoderLayer(d_model,
+                num_heads,
+                hidden_width
+            ) for _ in range(num_layers)
+        ])
+
+        self.query_seq = nn.Parameter(torch.randn(query_len, d_model), requires_grad=True)
+
+        self.q_position_embeddings = nn.Parameter(torch.randn(query_len, 1, self.d_head // 2 + 1), requires_grad=True)
+
+        self.register_buffer(
+            't_position_embeddings',
+            torch.arange(0, target_len)[:,None,None] / (10000 ** (2 * torch.arange(0, self.d_head // 2 + 1) / self.d_head)[None,None,:])
         )
 
-    def t_position_embedings(self, target_seq):
-
-        target_len = target_seq.size(1)
-        
-        position = torch.exp(
-                    1j * torch.arange(
-                        0,
-                        target_len,
-                        device=target_seq.device
-                    )[..., None, None] / (10000 ** (2 * torch.arange(0, self.d_head // 2 + 1) / self.d_head)[None,None,:])
-                )
-        
-        return position
-
-
     def forward(self, target_seq, padding_mask):
+        q_position_embeddings = torch.exp(1j * self.q_position_embeddings)
+        t_position_embeddings = torch.exp(1j * self.t_position_embeddings)
 
-        query_seq = self.query_seq.unsqueeze(0)
-
-        t_position_embedings = self.t_position_embedings(target_seq)
+        query_seq = self.query_seq.expand(*target_seq.shape)
 
         for layer in self.layers:
-
-            query_seq = layer(query_seq,
-                              target_seq,
-                              self.q_position_embeddings,
-                              t_position_embedings,
-                              padding_mask
-                              )
-            
+            query_seq = layer(
+                query_seq,
+                target_seq,
+                q_position_embeddings,
+                t_position_embeddings,
+                padding_mask
+            )
         return query_seq
